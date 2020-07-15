7634e40d06effd6677b4dcd9328c7dc9e66b35b9
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-07-15 02:18:55 -0400

    Models y BLL creada...

---
+++ AutosoftService/Model/Usuarios.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutosoftService.Model
+{
+    public class Usuarios
+    {
+        [Key]
+
+        public int UsuarioId { get; set; }
+        
+        [Required(ErrorMessage = "Debe ingresar su nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar su apellido")]
+        public string Apellidos { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar su email")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un Email valido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar su nombre se usuario")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Debe elegir un nivel de usuario.")]
+        public string NivelUsuario { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar una contraseña")]
+        [MinLength(5, ErrorMessage = "La contraseña debe contener al menos 5 caracteres.")]
+        [MaxLength(30, ErrorMessage = "La contraseña es muy larga.")]
+        public string Contrasena { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            Apellidos = string.Empty;
+            Email = string.Empty;
+            Usuario = string.Empty;
+            NivelUsuario = string.Empty;
+            Contrasena = string.Empty;
+            FechaIngreso = DateTime.Now;
+        }
+    }
+}
---
+++ AutosoftService/Model/Vehiculos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutosoftService.Model
+{
+    public class Vehiculos
+    {
+        [key]
+
+        public int VehiculoId { get; set; }
+        public int ClienteId { get; set; }
+        public string TipoVehiculo { get; set; }
+        public string Descripcion { get; set; }
+        public string Placa { get; set; }
+        public int Año { get; set; }
+        public DateTime Fecha { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public virtual Clientes Clientes { get; set; }
+
+        public Vehiculos()
+        {
+            VehiculoId = 0;
+            ClienteId = 0;
+            TipoVehiculo = string.Empty;
+            Descripcion = string.Empty;
+            Placa = string.Empty;
+            Año = 0;
+            Fecha = DateTime.Now;
+           
+        }
+    }
+}
