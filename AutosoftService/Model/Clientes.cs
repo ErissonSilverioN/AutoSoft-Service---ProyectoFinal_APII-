5e6d5d3e975002cc54edc7ba73065608ce36a356
Emerson Del Orbe Candelario <emerson_delorbe@ucne.edu.do>
2020-07-24 16:50:49 -0400

    arregloas en las Validaciones y en el diseno

--- AutosoftService/Model/Clientes.cs
+++ AutosoftService/Model/Clientes.cs
@@ -9,23 +9,38 @@ namespace AutosoftService.Model
     public class Clientes
     {
         [Key]
-
+       
+        [Required(ErrorMessage = "El campo ID no puede estar vació.")]
+        [Range(0, 1000000, ErrorMessage = "El campo ID no puede ser menor que 1 o mayor que 1000000.")]
         public int ClienteId { get; set; }
-
-        [Required(ErrorMessage ="Debe ingresar su nombre")]
+        [Required(ErrorMessage = "El campo nombre no puede estar vació.")]
+        [MinLength(3, ErrorMessage = "El campo lo minimo debe terner 3 caracteres.")]
+        [MaxLength(40, ErrorMessage = "El nombre es muy largo.")]
+        [RegularExpression(@"\S(.*)\S", ErrorMessage = "Debe ser un texto.")]
+      
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "Debe ingresar su cedula")]
+        [Required(ErrorMessage = "El campo cedula no debe estar vació.")]
+        [MinLength(11, ErrorMessage = "El Campo Cedula debe contener 11 Caracteres.")]
+     
         public string Cedula { get; set; }
 
-        [Required(ErrorMessage = "Debe ingresar su telefono")]
+        [Required(ErrorMessage = "El campo Teléfono no debe de estar vació.")]
+        [Phone(ErrorMessage = "Debes de ingresar tu número telefonico.")]
+        [MaxLength(10, ErrorMessage = "El campo telefono no tiene más de diez dígitos.")]
         public string Telefono { get; set; }
 
-        [Required(ErrorMessage = "Debe ingresar su direccion")]
-        public string Direccion { get; set; }
 
+        [Required(ErrorMessage = "El campo Dirección no debe de estar vació.")]
+        [MinLength(10, ErrorMessage = "La dirección es muy corta.")]
+        [MaxLength(100, ErrorMessage = "La dirección debe contener menos de 40 caracteres.")]
+        public string Direccion { get; set; }
         [Required(ErrorMessage = "Debe ingresar su email")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un Email valido.")]
         public string Email { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "El campo fecha no puede estar vacío.")]
+        [DisplayFormat(DataFormatString = "{0:dd,mm,yyyy}")]
         public DateTime Fecha { get; set; }
 
         public Clientes()
--- AutosoftService/Model/Proveedores.cs
+++ AutosoftService/Model/Proveedores.cs
@@ -9,26 +9,40 @@ namespace AutosoftService.Model
     public class Proveedores
     {
         [Key]
-
+        [Required(ErrorMessage = "El campo ID no puede estar vació.")]
+        [Range(0, 1000000, ErrorMessage = "El campo ID no puede ser menor que 1 o mayor que 1000000.")]
         public int ProveedorId { get; set; }
+        [Required(ErrorMessage = "El campo nombre no puede estar vació.")]
+        [MinLength(3, ErrorMessage = "El campo lo minimo debe terner 3 caracteres.")]
+        [MaxLength(40, ErrorMessage = "El nombre es muy largo.")]
+        [RegularExpression(@"\S(.*)\S", ErrorMessage = "Debe ser un texto.")]
 
-        [Required(ErrorMessage ="Debe ingresar su nombre")]
+       
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "Debe ingresar su telefono")]
-
+     
+        [Phone(ErrorMessage = "Debes de ingresar tu número telefonico.")]
+        [MaxLength(10, ErrorMessage = "El campo telefono no tiene más de diez dígitos.")]
         public string Telefono { get; set; }
 
         [Required(ErrorMessage = "Debe ingresar su email")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un Email valido.")]
 
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Debe ingresar su direccion")]
+        [Required(ErrorMessage = "El campo Dirección no debe de estar vació.")]
+        [MinLength(10, ErrorMessage = "La dirección es muy corta.")]
+        [MaxLength(100, ErrorMessage = "La dirección debe contener menos de 40 caracteres.")]
         public string Direccion { get; set; }
 
         [Required(ErrorMessage = "Debe ingresar su RNC")]
+
         public string RNC { get; set; }
+
         public int CantidadPedidos { get; set; }
+        [Required(ErrorMessage = "El campo fecha no puede estar vacío.")]
+        [DisplayFormat(DataFormatString = "{0:dd,mm,yyyy}")]
         public DateTime Fecha { get; set; }
 
         public Proveedores()
